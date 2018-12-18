d8dad73de9e15042d12b30713e37d63a320ad9f1
Joern Hinrichsen <joern.hinrichsen@ericsson.com>
2018-12-18 19:48:47 +0100

    minor

--- capsicain/capsicain.cpp
+++ capsicain/capsicain.cpp
@@ -18,7 +18,7 @@
 
 using namespace std;
 
-string version = "22";
+string version = "23";
 
 const bool START_AHK_ON_STARTUP = true;
 const int MAX_KEYMACRO_LENGTH = 10000;  //for testing; in real life, 100 keys = 200 up/downs should be enough
